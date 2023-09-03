741d5d7afce686ea2151db983576a465a67878af
sn706 <82053106+sn706@users.noreply.github.com>
2023-09-03 21:30:29 +0530

    script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function myFunction() { var input, filter, ul, li, a, i; input = document.getEle
         greet = 'Good Night';
 
     document.getElementById('wishes').innerHTML =
-    '<b>'+ 'Hi ' +   greet +   ' & Happy Onam </b>   '
+    '<b>'+ 'Hi ' +   greet +   '  </b>   '
     ;
 
 //last modified//
